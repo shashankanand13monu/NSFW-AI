469a3a6631a6fcc4b893b83ebc1e8ccdf58644b0
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-01-19 11:09:01 +0530

    Added try & Except

--- app.py
+++ app.py
@@ -41,64 +41,74 @@ if app_mode== 'About App':
     
 elif app_mode == 'Run Tests':
 
-    img_file_buffer = st.file_uploader("Upload an Image", type=["jpg","jpeg", "png"])
-    if img_file_buffer is not None:
-        imag = Image.open(img_file_buffer)
-    else:
-        demo_image= DEMO_IMAGE
-        imag= Image.open(demo_image)
+    try :
         
-    st.subheader('Predictions')
+        img_file_buffer = st.file_uploader("Upload an Image", type=["jpg","jpeg", "png"])
+        if img_file_buffer is not None:
+            imag = Image.open(img_file_buffer)
+        else:
+            demo_image= DEMO_IMAGE
+            imag= Image.open(demo_image)
+            
+        st.subheader('Predictions')
+            
+        kpi1, kpi2, kpi3 = st.columns(3)
+        with kpi1:
+            original_title = '<p style="text-align: center; font-size: 20px;"><strong>Label</strong></p>'
+            st.markdown(original_title, unsafe_allow_html=True)
+            kpi1_text = st.markdown ("0")
+        with kpi2:
+            original_title = '<p style="text-align: center; font-size: 20px;"><strong>Confidence</strong></p>'
+            st.markdown(original_title, unsafe_allow_html=True)
+            kpi2_text = st.markdown ("0")    # imag = Image.open(imag)
+        st.subheader('Input Image')
+        category,confidence = predict(imag)
         
-    kpi1, kpi2, kpi3 = st.columns(3)
-    with kpi1:
-        original_title = '<p style="text-align: center; font-size: 20px;"><strong>Label</strong></p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        kpi1_text = st.markdown ("0")
-    with kpi2:
-        original_title = '<p style="text-align: center; font-size: 20px;"><strong>Confidence</strong></p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        kpi2_text = st.markdown ("0")    # imag = Image.open(imag)
-    st.subheader('Input Image')
-    category,confidence = predict(imag)
-    
-    col1, col2,col3,col4 = st.columns(4)
-    original = imag
-    
-    original_title = '<p style="font-family:Arial Black; font-size: 40px;">Original</p>'
-    col1.markdown(original_title, unsafe_allow_html=True)
-    col1.image(original, use_column_width=True,width=256)
-# ---------------------------------------------------------------------
-    action1= '''<style >
-    p.detail { font-weight:bold;font-family:Arial Black;font-size:40px }
-    span.name { color:#06FD01;font-weight:bold;font-family:Tahoma;font-size:40px }
-    </style>  <p class="detail">Action: <span class="name">None</span> </p>'''
-    action2= '''<style >
-    p.detail { font-weight:bold;font-family:Arial Black;font-size:40px }
-    span.name { color:#FD0101;font-weight:bold;font-family:Tahoma;font-size:40px }
-    </style>  <p class="detail">Action: <span class="name">Blured</span> </p>'''
-# ---------------------------------------------------------------------
-       
-    if category=='Normal':
-        blurImage= imag
-        original_title = '<p style="font-family:Arial Black; color:#FD0177; font-size: 40px;">Pass</p>'
-        col2.markdown(action1, unsafe_allow_html=True)
-    else:   
-        blurImage = original.filter(ImageFilter.GaussianBlur(5))
-        original_title = '<p style="font-family:Arial Black; color:#FD0177; font-size: 40px;">Blurred</p>'
-       
-        col2.markdown(action2, unsafe_allow_html=True)
+        col1, col2,col3,col4 = st.columns(4)
+        original = imag
         
-    col2.image(blurImage, use_column_width=True,width=256) 
-    kpi1_text.write(f"<h1 style='text-align: center; color:red; '>{category}</h1>", unsafe_allow_html=True)
-    kpi2_text.write(f"<h1 style='text-align: center; color:red; '>{int(confidence)}%</h1>", unsafe_allow_html=True)    
-    st.subheader("Demo Images")
-    
-    filteredImages = ['00000215.jpg','00001670.jpg','underwearmens29.jpeg','hell.jpg','00000062.jpg','00000012.jpg'] # your images here
-    # caption = [] # your caption here
-    cols = cycle(st.columns(5)) # st.columns here since it is out of beta at the time I'm writing this
-    for idx, filteredImage in enumerate(filteredImages):
-        next(cols).image(filteredImage, width=150)
+        original_title = '<p style="font-family:Arial Black; font-size: 40px;">Original</p>'
+        col1.markdown(original_title, unsafe_allow_html=True)
+        col1.image(original, use_column_width=True,width=256)
+    # ---------------------------------------------------------------------
+        action1= '''<style >
+        p.detail { font-weight:bold;font-family:Arial Black;font-size:40px }
+        span.name { color:#06FD01;font-weight:bold;font-family:Tahoma;font-size:40px }
+        </style>  <p class="detail">Action: <span class="name">None</span> </p>'''
+        action2= '''<style >
+        p.detail { font-weight:bold;font-family:Arial Black;font-size:40px }
+        span.name { color:#FD0101;font-weight:bold;font-family:Tahoma;font-size:40px }
+        </style>  <p class="detail">Action: <span class="name">Blured</span> </p>'''
+    # ---------------------------------------------------------------------
+        
+        if category=='Normal':
+            blurImage= imag
+            original_title = '<p style="font-family:Arial Black; color:#FD0177; font-size: 40px;">Pass</p>'
+            col2.markdown(action1, unsafe_allow_html=True)
+        else:   
+            blurImage = original.filter(ImageFilter.GaussianBlur(5))
+            original_title = '<p style="font-family:Arial Black; color:#FD0177; font-size: 40px;">Blurred</p>'
+        
+            col2.markdown(action2, unsafe_allow_html=True)
+            
+        col2.image(blurImage, use_column_width=True,width=256) 
+        kpi1_text.write(f"<h1 style='text-align: center; color:red; '>{category}</h1>", unsafe_allow_html=True)
+        kpi2_text.write(f"<h1 style='text-align: center; color:red; '>{int(confidence)}%</h1>", unsafe_allow_html=True)    
+        st.subheader("Demo Images")
+        
+        filteredImages = ['00000215.jpg','00001670.jpg','underwearmens29.jpeg','hell.jpg','00000062.jpg','00000012.jpg'] # your images here
+        # caption = [] # your caption here
+        cols = cycle(st.columns(5)) # st.columns here since it is out of beta at the time I'm writing this
+        for idx, filteredImage in enumerate(filteredImages):
+            next(cols).image(filteredImage, width=150)
+    except:
+        st.subheader("Oops, Image not compatible")
+        st.subheader("Dmo Images : ")
+        filteredImages = ['00000215.jpg','00001670.jpg','underwearmens29.jpeg','hell.jpg','00000062.jpg','00000012.jpg'] # your images here
+        # caption = [] # your caption here
+        cols = cycle(st.columns(5)) # st.columns here since it is out of beta at the time I'm writing this
+        for idx, filteredImage in enumerate(filteredImages):
+            next(cols).image(filteredImage, width=150)
 # ---------------------------------------------------------------------
 elif app_mode== 'Show Code':
     
